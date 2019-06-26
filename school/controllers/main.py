6fedd732ba3594fb7b8257b1ba926773c4aa3d57
Damien Bouvy <dbo@odoo.com>
2019-06-26 17:02:57 +0200

    Revert "Chapter 1 - Revisiting the basics"
    This reverts commit e617d77e44401704bfcc892b8c54a6ab53731b74.

--- school/controllers/main.py
+++
@@ -1,7 +0,0 @@
-from odoo.http import Controller, request, route
-
-
-class SchoolController(Controller):
-    @route('/school/group/<model("school.group"):group_id>', auth='user')
-    def group_list(self, group_id):
-        return request.render('school.class_list', {'group_id': group_id})
--- school/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import res_partner
-from . import school_group
-from . import school_option
